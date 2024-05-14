583129b88e2f5296c155a2d0db390e15bc5bdfbc
osekine <osekine42@gmail.com>
2024-05-14 15:54:22 +0300

    P1431 Solved

--- bin/1431. Kids With the Greatest Number of Candies.dart
+++ bin/1431. Kids With the Greatest Number of Candies.dart
@@ -1,11 +1,22 @@
 class Solution {
   List<bool> kidsWithCandies(List<int> candies, int extraCandies) {
-    return [];
+    List<bool> ans = [];
+
+    int max = 0;
+    candies.forEach((element) {
+      if (element > max) max = element;
+    });
+
+    for (int i = 0; i < candies.length; i++) {
+      ans.add(candies[i] + extraCandies >= max);
+    }
+
+    return ans;
   }
 }
 
 void main() {
   Solution sol = Solution();
 
-  print(sol.kidsWithCandies([], 0));
+  print(sol.kidsWithCandies([2, 3, 5, 1, 3], 3));
 }
