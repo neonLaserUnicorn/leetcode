5fff19b711d5f4a202dc4d84ae3d67d822db51f7
osekine <osekine42@gmail.com>
2024-05-14 15:30:46 +0300

    init commit

---
+++ bin/leet_code_problems.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+class Solution {
+  String gcdOfStrings(String str1, String str2) {
+    String ans = 'a';
+
+    return ans;
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+  print('start');
+  String str1 = stdin.readLineSync()!;
+  // String str2 = stdin.readLineSync()!;
+  print(sol.gcdOfStrings(str1, 'str2'));
+}
