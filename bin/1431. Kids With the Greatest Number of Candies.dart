730058bd13c75579a0b224490e26fd0d02682d2e
osekine <osekine42@gmail.com>
2024-05-14 15:48:52 +0300

    P1431 Started

---
+++ bin/1431. Kids With the Greatest Number of Candies.dart
@@ -0,0 +1,11 @@
+class Solution {
+  List<bool> kidsWithCandies(List<int> candies, int extraCandies) {
+    return [];
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+
+  print(sol.kidsWithCandies([], 0));
+}
