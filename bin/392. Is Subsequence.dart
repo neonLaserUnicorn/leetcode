842bbf6bbce60c6a2726487ee5d0356a10c6ece6
neonLaserUnicorn <astrargot@gmail.com>
2024-05-16 21:54:37 +0300

    P392 Solved

--- bin/392. Is Subsequence.dart
+++ bin/392. Is Subsequence.dart
@@ -1,11 +1,18 @@
 class Solution {
   bool isSubsequence(String s, String t) {
-    return false;
+    if (s.length > t.length) return false;
+    int i = 0;
+    for (int j = 0; j < t.length; j++) {
+      if (i < s.length && s[i] == t[j]) {
+        ++i;
+      }
+    }
+    return i == s.length;
   }
 }
 
 void main() {
   Solution sol = Solution();
 
-  print(sol.isSubsequence("abc", "ahbgdc"));
+  print(sol.isSubsequence("abc", "ahbgd"));
 }
