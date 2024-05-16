edbf783606288af7cfcb236fc4a021c8a4ed5915
neonLaserUnicorn <astrargot@gmail.com>
2024-05-16 21:50:45 +0300

    P392 Started

---
+++ bin/392. Is Subsequence.dart
@@ -0,0 +1,11 @@
+class Solution {
+  bool isSubsequence(String s, String t) {
+    return false;
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+
+  print(sol.isSubsequence("abc", "ahbgdc"));
+}
