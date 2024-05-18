856c566e29e096e899b1773350c767ad03ddf1fc
neonLaserUnicorn <astrargot@gmail.com>
2024-05-18 17:53:05 +0300

    P2215 Solved

--- bin/2215. Find the Difference of Two Arrays.dart
+++ bin/2215. Find the Difference of Two Arrays.dart
@@ -1,6 +1,12 @@
 class Solution {
   List<List<int>> findDifference(List<int> nums1, List<int> nums2) {
-    return [];
+    Set<int> set1 = nums1.toSet();
+    Set<int> set2 = nums2.toSet();
+    List<List<int>> ans = [];
+
+    ans.add(nums1.where((element) => !set2.contains(element)).toSet().toList());
+    ans.add(nums2.where((element) => !set1.contains(element)).toSet().toList());
+    return ans;
   }
 }
 
