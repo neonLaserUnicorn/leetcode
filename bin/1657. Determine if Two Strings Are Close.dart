e6f7fcfe8013cb536749847e9c7216a617012776
neonLaserUnicorn <astrargot@gmail.com>
2024-05-18 17:55:33 +0300

    P1657 Started

---
+++ bin/1657. Determine if Two Strings Are Close.dart
@@ -0,0 +1,10 @@
+class Solution {
+  bool closeStrings(String word1, String word2) {
+    return false;
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+  print(sol.closeStrings("abc", "bca"));
+}
