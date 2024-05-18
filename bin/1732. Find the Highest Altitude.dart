2b21f937e60fc53709801a6dbaefcdd38b35d50d
neonLaserUnicorn <astrargot@gmail.com>
2024-05-18 17:36:52 +0300

    P1732 Started

---
+++ bin/1732. Find the Highest Altitude.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int largestAltitude(List<int> gain) {
+    int max = 0;
+
+    return max;
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+
+  print(sol.largestAltitude([-5, 1, 5, 0, -7]));
+}
