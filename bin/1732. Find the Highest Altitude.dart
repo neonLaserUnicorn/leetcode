4ececfa97c095277a4ec88ab26f4e72e2ed95cae
neonLaserUnicorn <astrargot@gmail.com>
2024-05-18 17:38:17 +0300

    P1732 Solved

--- bin/1732. Find the Highest Altitude.dart
+++ bin/1732. Find the Highest Altitude.dart
@@ -1,7 +1,12 @@
 class Solution {
   int largestAltitude(List<int> gain) {
     int max = 0;
+    int cur = 0;
 
+    for (int i = 0; i < gain.length; ++i) {
+      cur += gain[i];
+      if (cur > max) max = cur;
+    }
     return max;
   }
 }
@@ -9,5 +14,5 @@ class Solution {
 void main() {
   Solution sol = Solution();
 
-  print(sol.largestAltitude([-5, 1, 5, 0, -7]));
+  print(sol.largestAltitude([-4, -3, -2, -1, 4, 3, 2]));
 }
