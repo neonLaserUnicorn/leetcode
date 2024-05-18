4dca6cd94cfe10b4ba8ed98f58b2d5f932e367cf
neonLaserUnicorn <astrargot@gmail.com>
2024-05-18 17:44:02 +0300

    P724 Solved

--- bin/724. Find Pivot Index.dart
+++ bin/724. Find Pivot Index.dart
@@ -1,10 +1,23 @@
 class Solution {
   int pivotIndex(List<int> nums) {
+    int left = 0;
+    int right = 0;
+    for (int i = nums.length - 1; i > 0; i--) {
+      right += nums[i];
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (left == right) return i;
+      left += nums[i];
+      if (i < nums.length - 1)
+        right -= nums[i + 1];
+      else
+        right = 0;
+    }
     return -1;
   }
 }
 
 void main() {
   Solution sol = Solution();
-  print(sol.pivotIndex([1, 7, 3, 6, 5, 6]));
+  print(sol.pivotIndex([1, 2, -3, 6]));
 }
