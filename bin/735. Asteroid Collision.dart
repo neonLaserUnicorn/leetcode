a08106a7e10f40eed73c64cf8adeb654a980654a
neonLaserUnicorn <astrargot@gmail.com>
2024-05-20 11:21:41 +0300

    P735 Started

---
+++ bin/735. Asteroid Collision.dart
@@ -0,0 +1,11 @@
+class Solution {
+  List<int> asteroidCollision(List<int> asteroids) {
+    return [];
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+
+  print(sol.asteroidCollision([5, 10, -5]));
+}
