3f33cdbc6bf3847275496f65d21e1be0b0156c85
neonLaserUnicorn <astrargot@gmail.com>
2024-05-23 10:52:20 +0300

    P649 Solved

--- bin/649. Dota2 Senate.dart
+++ bin/649. Dota2 Senate.dart
@@ -1,8 +1,36 @@
 class Solution {
   String predictPartyVictory(String senate) {
     final ans = {'R': 'Radiant', 'D': 'Dire'};
+    int rBans = 0;
+    int dBans = 0;
+    String tmp = '';
+    bool isWin = false;
+    while (!isWin) {
+      for (int i = 0; i < senate.length; i++) {
+        if (senate[i] == 'R') {
+          if (dBans > 0) {
+            --dBans;
+            continue;
+          }
+          ++rBans;
+        }
+        if (senate[i] == 'D') {
+          if (rBans > 0) {
+            --rBans;
+            continue;
+          }
+          ++dBans;
+        }
+        tmp += senate[i];
+      }
+      senate = tmp;
+      tmp = '';
+      if (!senate.contains('R') || !senate.contains('D')) {
+        isWin = true;
+      }
+    }
 
-    return ans['R']!;
+    return ans[senate[0]]!;
   }
 }
 
