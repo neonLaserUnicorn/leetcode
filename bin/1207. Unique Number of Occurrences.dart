ef3814150e8b9bd043d88bed07d2edd9f5cd8b15
neonLaserUnicorn <astrargot@gmail.com>
2024-05-28 10:16:51 +0300

    P1207 Started

---
+++ bin/1207. Unique Number of Occurrences.dart
@@ -0,0 +1,13 @@
+class Solution {
+  bool uniqueOccurrences(List<int> arr) {
+    bool ans = false;
+
+    return ans;
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+
+  print(sol.uniqueOccurrences([1, 2, 2, 1, 1, 3]));
+}
