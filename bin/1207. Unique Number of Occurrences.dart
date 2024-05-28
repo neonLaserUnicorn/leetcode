5f024699cdda4742b35a00a300e3896102043d51
neonLaserUnicorn <astrargot@gmail.com>
2024-05-28 10:19:26 +0300

    P1207 Solved

--- bin/1207. Unique Number of Occurrences.dart
+++ bin/1207. Unique Number of Occurrences.dart
@@ -1,7 +1,18 @@
 class Solution {
   bool uniqueOccurrences(List<int> arr) {
     bool ans = false;
-
+    Map<int, int> repeats = {};
+    for (int i = 0; i < arr.length; i++) {
+      repeats.update(
+        arr[i],
+        (val) => val + 1,
+        ifAbsent: () => 1,
+      );
+    }
+    Set<int> set = repeats.values.toSet();
+    if (set.length == repeats.length) {
+      ans = true;
+    }
     return ans;
   }
 }
@@ -9,5 +20,5 @@ class Solution {
 void main() {
   Solution sol = Solution();
 
-  print(sol.uniqueOccurrences([1, 2, 2, 1, 1, 3]));
+  print(sol.uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]));
 }
